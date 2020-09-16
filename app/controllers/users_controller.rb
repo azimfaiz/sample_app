b535d1005dc00716a32b87dcaa5fbdc79545914d
azimfaiz <azimfaiz271197@gmail.com>
2020-09-16 12:21:14 +0800

    chapter_8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,3 @@
-
 class UsersController < ApplicationController
   def show
     @user = User.find_by id: params[:id]
@@ -11,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
